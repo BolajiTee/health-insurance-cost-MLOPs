7d25fcd06f5a579dd53c8c3cccf8512be23aa176
Bolaji Tunde <bolajitunde16@gmail.com>
2025-12-09 10:41:41 +0100

    added random state to params

--- utils.py
+++ utils.py
@@ -62,12 +62,14 @@ def params():
                     'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
                     # 'splitter':['best','random'],
                     # 'max_features':['sqrt','log2'],
+                    'random_state': 0
                 },
                 "Random Forest":{
                     # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
                  
                     # 'max_features':['sqrt','log2',None],
-                    'n_estimators': [8,16,32,64,128,256]
+                    'n_estimators': [8,16,32,64,128,256],
+                    'random_state': 0
                 },
                 "Gradient Boosting":{
                     # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
@@ -75,22 +77,26 @@ def params():
                     'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
                     # 'criterion':['squared_error', 'friedman_mse'],
                     # 'max_features':['auto','sqrt','log2'],
-                    'n_estimators': [8,16,32,64,128,256]
+                    'n_estimators': [8,16,32,64,128,256],
+                    'random_state': 0
                 },
                 "Linear Regression":{},
                 "XGBRegressor":{
                     'learning_rate':[.1,.01,.05,.001],
-                    'n_estimators': [8,16,32,64,128,256]
+                    'n_estimators': [8,16,32,64,128,256],
+                    'random_state': 0
                 },
                 "CatBoosting Regressor":{
                     'depth': [6,8,10],
                     'learning_rate': [0.01, 0.05, 0.1],
-                    'iterations': [30, 50, 100]
+                    'iterations': [30, 50, 100],
+                    'random_state': 0
                 },
                 "AdaBoost Regressor":{
                     'learning_rate':[.1,.01,0.5,.001],
                     # 'loss':['linear','square','exponential'],
-                    'n_estimators': [8,16,32,64,128,256]
+                    'n_estimators': [8,16,32,64,128,256],
+                    'random_state': 0
                 }
                 
             }
